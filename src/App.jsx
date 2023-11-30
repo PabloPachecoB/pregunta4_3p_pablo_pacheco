c361b55a0b8007e8325bc142f078f66b2c7776d3
PabloPachecoB <pablitopacheco051@gmail.com>
2023-11-30 15:27:55 -0400

    me olvide subir mi commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './view/home/Home';
+import Dashboard from './view/dashboard/Dashboard';
+import About from './view/about/About';
+
+import Header from './componentes/header/Header';
 
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
+
+      <BrowserRouter>
+            <Header/>
+
+
+            <Routes>
+              <Route path="/"/>
+              <Route path="/aboute"element={<About/>} />
+
+              <Route path="/home"element={<Home/>} />
+              <Route path="/dashboard"element={<Dashboard/>} />
+
+            </Routes>
+
+      
+            </BrowserRouter>
+        
 
       </div>
      
