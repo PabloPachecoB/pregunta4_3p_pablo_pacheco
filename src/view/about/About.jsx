9f39a6d2ed58d0004b38d91174468e40d083a60d
PabloPachecoB <pablitopacheco051@gmail.com>
2023-11-30 11:36:23 -0400

    creando mi web

---
+++ src/view/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div>
+        <h2>estamos en la pantalla About</h2>
+    </div>
+  )
+}
+
+export default About
---
+++ src/view/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h2>estamos en la pantalla Dashboard</h2>
+    </div>
+  )
+}
+
+export default Dashboard
